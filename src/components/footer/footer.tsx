8ab6cb4a6646767cde423e6b9231fac7974e89e5
Gleydsong <ggleydson@icloud.com>
2026-06-03 22:07:33 +0100

    Refactor Header and Footer components to use Logo component instead of Image, improving code consistency and maintainability.

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,14 +1,12 @@
 import Link from "next/link";
-import Image from "next/image";
+import { Logo } from "@/components/logo";
 
 export const Footer = () => {
   return (
     <footer className="border-t border-white/10 ">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col justify-between md:flex-row  md:justify-start gap-8 py-8">
-          <Link rel="stylesheet" href="/">
-            <Image src="/logo.svg" alt="Logo" width={116} height={32} />
-          </Link>
+          <Logo />
 
           <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-muted-foreground not-even:">
             <Link
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,27 +1,26 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ActiveLink } from "../active-link";
+import { Logo } from "../logo";
 
 export const Header = () => {
-    
-
   return (
     <header className="bg-red-500">
-        <div className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div className="flex h-16 items-center justify-between">
-                    <Link href="/" > Logo</Link>
+      <div className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <Logo />
 
-                    <nav className="flex items-center gap-6"> 
-                        <ActiveLink href="/" > Inicio</ActiveLink>
-                        <ActiveLink href="/blog" > Blog</ActiveLink>
-                            <Button variant="default">
-                            <Link href="começar">Começar</Link>
-                        </Button>
-                    </nav>
-                </div>
-            </div>
+            <nav className="flex items-center gap-6">
+              <ActiveLink href="/"> Inicio</ActiveLink>
+              <ActiveLink href="/blog"> Blog</ActiveLink>
+              <Button variant="default">
+                <Link href="começar">Começar</Link>
+              </Button>
+            </nav>
+          </div>
         </div>
+      </div>
     </header>
   );
 };
