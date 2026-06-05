9b4bfb85970688ec5309ae2483c124cc9cafd166
Gleydsong <ggleydson@icloud.com>
2026-06-05 21:02:11 +0100

    Add title attribute to Logo component link for improved accessibility

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const Logo = () => {
   return (
-    <Link href="/">
+    <Link href="/" title="pagina inicial">
       <Image src="/logo.svg" alt="Logo" width={116} height={32} />
     </Link>
   );
