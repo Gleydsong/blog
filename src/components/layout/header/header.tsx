5576ec2c5a8099082e5f887fd258af9aa965464a
Gleydsong <ggleydson@icloud.com>
2026-06-06 20:00:12 +0100

    Remove CallToAction and SupportSection components to streamline the codebase and eliminate unused files.

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { ActiveLink } from "../active-link";
-import { Logo } from "../logo";
+import { ActiveLink } from "../../active-link";
+import { Logo } from "../../logo";
 
 export const Header = () => {
   return (
